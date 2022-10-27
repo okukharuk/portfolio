7e6bbad13fe62c73162d4ed27d2e17b9d4b413f7
VipDipp <kuxaruk03@ukr.net>
2022-10-27 23:14:44 +0300

    feat: drop svg

--- src/components/AnimatedText.tsx
+++ src/components/AnimatedText.tsx
@@ -2,6 +2,8 @@ import { motion, useAnimation } from 'framer-motion';
 import React from 'react';
 import { useEffect } from 'react';
 
+import drop from '../public/svgs/drop.svg';
+
 /*const Title = styled.h2`
   font-size: 3rem;
   font-weight: 600;
@@ -26,13 +28,6 @@ export const AnimatedText = () => {
   const wordBreakAnimation = {
     hidden: {},
     visible: {},
-    textRed: {
-      color: ["#000", "#ff0000"],
-      transition: {
-        duration: 1,
-        ease: [0.2, 0.65, 0.3, 0.9],
-      },
-    },
   };
 
   const characterClockAnimation = {
@@ -63,6 +58,45 @@ export const AnimatedText = () => {
         ease: [0.2, 0.65, 0.3, 0.9],
       },
     },
+    textRed: {
+      WebkitTextFillColor: "transparent",
+      backgroundImage: [
+        "linear-gradient(180deg,#ff0000 0%,#000 0%)",
+        "linear-gradient(180deg,#ff0000 100%,#000 0%)",
+        "linear-gradient(180deg,#ff0000 100%,#000 100%)",
+      ],
+      transition: {
+        duration: 4,
+      },
+    },
+  };
+
+  const dropAnimation = {
+    dropFall: {
+      opacity: 1,
+      y: "8rem",
+      transition: {
+        duration: 10,
+        ease: [0.2, 0.65, 0.3, 0.9],
+      },
+    },
+  };
+
+  const dropPathAnimation = {
+    hidden: {
+      opacity: 0,
+    },
+    dropShow: {
+      opacity: 1,
+    },
+    dropFall: {
+      y: "8rem",
+      height: "8rem",
+      transition: {
+        duration: 10,
+        ease: [0.2, 0.65, 0.3, 0.9],
+      },
+    },
   };
 
   React.useEffect(() => {
@@ -73,7 +107,11 @@ export const AnimatedText = () => {
       ctrls.start("visibleBreak");
       setTimeout(() => {
         ctrls.start("textRed");
-      }, 1000);
+      }, 2000);
+      setTimeout(() => {
+        ctrls.start("dropShow");
+        ctrls.start("dropFall");
+      }, 6000);
     }, 2500);
   }, []);
 
@@ -93,28 +131,45 @@ export const AnimatedText = () => {
           {character}
         </motion.span>
       ))}
-      {breakText.split(" ").map((word, index) => {
+      {breakText.split(" ").map((word, indexWord) => {
         return (
           <motion.span
             aria-hidden="true"
-            key={index}
+            key={indexWord}
             initial="hidden"
             animate={ctrls}
             variants={wordBreakAnimation}
             transition={{
-              delayChildren: index * 0.25,
+              delayChildren: indexWord * 0.25,
               staggerChildren: 0.05,
             }}
             className="ml-4 text-9xl"
           >
-            {word.split("").map((character, index) => {
+            {word.split("").map((character, indexChar) => {
               return (
                 <motion.span
                   aria-hidden="true"
-                  key={index}
+                  key={indexChar}
                   variants={characterBreakAnimation}
+                  style={{
+                    WebkitBackgroundClip: "text",
+                  }}
                 >
                   {character}
+                  {indexWord == 0 && indexChar == 0 && (
+                    <motion.div
+                      className="fixed w-[0.5rem] h-[1.8rem] ml-[9.1rem] bottom-[4.6rem] bg-[#ff0000] overflow-visible"
+                      variants={dropPathAnimation}
+                    >
+                      <motion.img
+                        src={drop}
+                        animate={ctrls}
+                        initial="hidden"
+                        className="mt-2 min-w-[1.5rem] right-[-0.48rem] fixed"
+                        variants={dropAnimation}
+                      />
+                    </motion.div>
+                  )}
                 </motion.span>
               );
             })}
